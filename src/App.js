01ad015f2d3ed86f948b80d23db8ba9efd80e192
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-10 11:32:28 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react"
-import './App.css';
+import React, { useEffect, useState } from "react"
+import axios from "axios";
 
 function App() {
+  //array json object,json object
+  //const [users,setUsers]=useState([]);
+  const [users,setUsers]=useState("");
+  useEffect(()=>{
+    let Tast=async()=>{
+      // let response=await axios.get('https://jsonplaceholder.typicode.com/todos/')
+      let response=await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      setUsers(response.data);
+    }
+    Tast()
+    // axios.get('https://jsonplaceholder.typicode.com/todos/').then((res)=>{
+    //   setUsers(res.data);
+    // })
+  },[])
   return (
     <div className="App">
-      
+      {/* {users&&(users.map((user=>(
+        <div key={user.id}>
+          <p>{user.title}</p>
+        </div>
+      ))))} */}
+      {users&&<div>{users.time.updated}</div>}
     </div>
   );
 }
